0f671d90d82a0eea1482bddaef0be714e41187b1
Linden Chiu <hchiu87@gmail.com>
2018-11-02 01:16:39 -0700

    implemented delete feature

--- app.js
+++ app.js
@@ -1,33 +1,81 @@
-/*
-
- */
-var curKeyValue = 0; 
-var newKeyValue = curKeyValue.toString();
+var currentKey;
 var cardInfo = {};
+
+var renderDisplay = function(storage) {
+  
+  for (var key in storage) {
+    
+    var $vocabDiv = $('<div class="vocab"></div>');
+    //console.log(storage[key]);
+    
+    if(storage.hasOwnProperty(key)) {
+      var check = JSON.parse(storage[key]);
+      var firstKey = Object.keys(check);
+    } else {
+        break;
+    }
+    
+    if (check[firstKey] != undefined) {
+      $vocabDiv.html(
+        '<span class="kanji">' + firstKey  + '</span>' + " " +  " - " + '<span class="definition">' + check[firstKey] + '</span>' + " "
+        + '<button class=" del-item" number=' + key + '>Delete</button>'
+      ); 
+
+
+      $(".show-text .data-entries").append($vocabDiv);
+    } else {
+        continue;
+    }
+  }
+
+}
+
+var editFeature = function(name) {
+  var newVal = prompt("Please make a change");
+  var oldVal = name;
+  if(newVal === null) {
+    return oldVal;
+  }
+  if(newVal !== '') {
+    return newVal;
+  }
+  return newVal + editFeature(oldVal);
+}
 
 $(document).ready(function(){
   console.log("before\n", window.localStorage);
-  // add event listener
+  renderDisplay(localStorage);
+  
   $(".add-text-btn").on("click", function(){
-    //$(".show-text").empty();
-
+    //console.log(curKeyValue);
+    if (localStorage["nextKey"] === undefined) { 
+      currentKey = 1;
+    } else {
+      currentKey = localStorage["nextKey"]
+    }
     var $value = $('#theValue')
-    var $definition = $('#theValue')
+    var $definition = $('#theDefinition')
     var curTextValue = $('#theValue').val(); // reading from <input>
     var curDefValue = $('#theDefinition').val();
+
       if (curTextValue.length === 0 || curDefValue.length === 0) {
         alert ('Please fill out both fields')
       } else {
+
           cardInfo[curTextValue] = curDefValue;
           var test = JSON.stringify(cardInfo);
-          window.localStorage.setItem(newKeyValue, test);
-          var vocab = $('<li></li>')
-          vocab.text(curTextValue + " - " + curDefValue)
-          $(".data-entries").append(vocab);
-          curKeyValue++;
-          newKeyValue = curKeyValue; 
-          $value.empty();
-          $definition.empty();
+          var $vocabDiv = $('<div class="vocab"></div>');
+
+          localStorage.setItem(currentKey, test);
+          console.log(localStorage)
+          $(".show-text .data-entries").empty();
+          renderDisplay(localStorage);
+
+          currentKey++;
+          localStorage.setItem("nextKey", currentKey);
+          console.log(currentKey);
+          $value.val("");
+          $definition.val("");
           cardInfo = {};
         }
     
@@ -36,25 +84,54 @@ $(document).ready(function(){
   // remove item from app
   //clear space for new addition
   $(".clear-previous-btn").on("click", function(){
-     $(".show-text").empty();
+     $(".show-text .data-entries").empty();
   });
 
 
   //button to show previous entries in local storage
   $(".show-previous-btn").on("click", function(){
-     $(".show-text").empty();
-    var lsDisplay = JSON.stringify(localStorage);
-    $(".show-text").append(lsDisplay);
+    $(".show-text .data-entries").empty();
+    renderDisplay(localStorage);
   });
 
-  // listen for click event (del)
+  //EDITING FUNCTION
+
+  $('.vocab').children('.kanji').click(function(){
+    var $vocab = $(this)[0].innerText;
+    console.log($(this).parent());
+      
+      //var name = this.text;
+      //console.log(name) 
+     
+    })
+
+   $('.vocab').children('.definition').click(function(){
+    var $vocab = $(this)[0].innerText;
+    console.log($vocab);
+      
+      //var name = this.text;
+      //console.log(name) 
+     
+    })
+
+//DELETE INDIVIDUAL ENTRY
+
+$(".show-text .data-entries").on("click", ".del-item", function() {
+     console.log($(this))
+     var numberKey = $(this).attr('number') 
+      alert (numberKey);
+      $(".show-text .data-entries").empty();
+      localStorage.removeItem(numberKey);
+      renderDisplay(localStorage);
+    // $("#theKey").val(''); //clears input box
+  });   
+
+//wipe local storage
   $(".clear-cache-btn").on("click", function(){
     // clear local storage
     localStorage.clear();
-    curKeyValue = "0"; 
+    currentKey = 0; 
     $(".show-text").empty();
   });
-
-  //
 
 });
